f559b22433034bcc94df1e962401dcf1643c0ba7
Lucas <lucasciappa97@gmail.com>
2022-07-25 00:10:33 -0300

    config things

--- next.config.js
+++ next.config.js
@@ -1,14 +1,32 @@
-
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  /* config options here */
   reactStrictMode: true,
-},
-{
   publicRuntimeConfig: {
-      APP_NAME: "Neucrist",
-      API_DEVELOPMENT: "http://localhost:8000/api",
-      PRODUCTION: false,
-      DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-      DOMAIN_PRODUCTION: 'https://neucrist.com'
-  },
+    APP_NAME: "Neucrist",
+    API_DEVELOPMENT: "http://localhost:8000/api",
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+    DOMAIN_PRODUCTION: 'https://neucrist.com'
+},
   webpack5: false,
 }
+
+module.exports = nextConfig
+
+
+// module.exports = {
+//   reactStrictMode: true,
+// },
+// {
+//   publicRuntimeConfig: {
+//       APP_NAME: "Neucrist",
+//       API_DEVELOPMENT: "http://localhost:8000/api",
+//       PRODUCTION: false,
+//       DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+//       DOMAIN_PRODUCTION: 'https://neucrist.com'
+//   },
+//   webpack5: false,
+// }
